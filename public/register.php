3dc8f5858e96cfc75d34aabab87ec3a0e500aa96
Eder Luiz <160525399+Luiz-Eder@users.noreply.github.com>
2025-10-14 12:09:39 -0300

    Update register.php

--- public/register.php
+++ public/register.php
@@ -9,58 +9,35 @@ use App\Domain\UserValidator;
 use App\Infra\FileUserRepository;
 
 $file = __DIR__ . '/../storage/users.txt';
-$userRepository = new FileUserRepository($file);
-$userValidator = new UserValidator(); 
-$userService = new UserService($userRepository, $userValidator);
 
-$message = '';
-$errors = [];
+$service = new UserService(new FileUserRepository($file), new UserValidator);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $response = $userService->register($_POST);
-    
-    if ($response['success']) {
-        header('Location: users.php'); 
-        exit;
-    } else {
-        $message = 'Falha no cadastro.';
-        $errors = $response['errors'] ?? []; 
-    }
-}
+$response = $service->register($_POST);
+
+$message = $response ? 'Usuário cadastrado com sucesso.' : 'Falha no cadastro.';
 
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Cadastro de Usuários</title>
+    <title>Resultado</title>
 </head>
 <body>
-    <h1>Cadastro de Usuários</h1>
-
-    <?php if (!empty($message) && empty($errors)): ?>
-        <p style="color: green; font-weight: bold;">
-            <?= htmlspecialchars($message) ?>
-        </p>
-    <?php elseif (!empty($errors)): ?>
-        <p style="color: red; font-weight: bold;">
-            <?= htmlspecialchars($message) ?> Detalhes:
-        </p>
-        <ul style="color: red;">
-            <?php foreach ($errors as $error): ?>
-                <li><?= htmlspecialchars($error) ?></li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
-
-    <form method="POST">
-        Nome: <input type="text" name="name" value="<?= htmlspecialchars($_POST['name'] ?? '') ?>" required><br><br>
-        E-mail: <input type="email" name="email" value="<?= htmlspecialchars($_POST['email'] ?? '') ?>" required><br><br>
-        Senha: <input type="password" name="password" required><br><br>
-        <button type="submit">Cadastrar</button>
-    </form>
     
-    <p><a href="users.php">Ver Listagem de Usuários</a></p>
+    <p>
+        <?php if ($response): ?>
+            <span style="color: green; font-weight: bold;"><?= htmlspecialchars($message) ?></span>
+        <?php else: ?>
+            <span style="color: red; font-weight: bold;"><?= htmlspecialchars($message) ?></span>
+        <?php endif; ?>
+    </p>
+
+    <p>
+        <a href="index.php">Fazer Novo Cadastro</a>
+        |
+        <a href="users.php">Ver Lista de Usuários</a>
+    </p>
 
 </body>
 </html>
