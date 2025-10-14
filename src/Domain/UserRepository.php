220303dab8c42a7453b7930c5c3428a528d94ed5
Eder Luiz <160525399+Luiz-Eder@users.noreply.github.com>
2025-10-14 12:10:23 -0300

    Update UserRepository.php

--- src/Domain/UserRepository.php
+++ src/Domain/UserRepository.php
@@ -10,4 +10,9 @@ interface UserRepository
      * @param array{name:string,email:string,password:string} $user
      */
     public function save(array $user): void;
+
+    /**
+     * @return array<int, array{name:string, email:string, password:string}>
+     */
+    public function findAll(): array;
 }
